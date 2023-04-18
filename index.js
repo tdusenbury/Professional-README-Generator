d42d554aa7e0c2f3f032e56b44905b6127255982
The Duz <tamara.dusenbury@gmail.com>
2023-04-18 12:19:45 -0700

    installed inquirer

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
 
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: 'ENTER QUESTION HERE? (Required)'
+    validate: nameOfTheThing => {
+        if (nameOfTheThing) {
+            return true;
+        } else {
+            console.log('Please enter WHAT QUESTION YOU WANT ANSWERED')
+        }
+    }
+},
+{
+    type: 'input',
+    name: (SEE ABOVE)
+}]
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
